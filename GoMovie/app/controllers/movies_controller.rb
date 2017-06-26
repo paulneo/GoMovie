186f153118a271ba47e485cc12d5a65edf261ae8
paulneo <localos2000@gmail.com>
2017-06-25 19:13:11 -0500

    añadiendo vista new y añdiendo a controller new y create

--- GoMovie/app/controllers/movies_controller.rb
+++ GoMovie/app/controllers/movies_controller.rb
@@ -6,4 +6,17 @@ class MoviesController < ApplicationController
     @movie = Movie.find(params[:id])
 
   end
+  def create
+    @movie =  Movie.new(movie_params)
+    if @movie.save
+      redirect_to admin_movies_path
+    else
+      render new_admin_movie_path
+    end
+  end
+  private
+
+  def movie_params
+    params.require(:movie).permit(:name,:category,:gender,:sinopsys)
+  end
 end
--- GoMovie/app/models/movie.rb
+++ GoMovie/app/models/movie.rb
@@ -1,4 +1,5 @@
 class Movie < ApplicationRecord
+  validate_presence_of :name, :category,:gender,:sinopsys ,:image
   belongs_to :category
   belongs_to :gender
 end
